9deab0104b92062c3578863f50175781854cc60d
Siddharth Kannan <kannan.siddharth12@gmail.com>
2015-12-04 15:33:15 +0530

    Add detailed statistics reporting
    - time taken
    - percentage done
    - time remaining
    
    Signed-off-by: Siddharth Kannan <kannan.siddharth12@gmail.com>

--- fileHandlerForNNOutput-1.py
+++ fileHandlerForNNOutput-1.py
@@ -45,12 +45,14 @@ def neuralNetwork(data):
 def storeBGR2Output(start_index, end_index):
         start_time = time.time()
         step_2_times = []
-	AllOutputs = open(str(start_index) + "-" + str(end_index) + ".txt",'w');
+        main_file_name = 'main_' + str(start_index) + "-" + str(end_index) + ".txt"
+	AllOutputs = open(main_file_name, 'w');
+        op_start_time = time.time()
 	for i in range(start_index, end_index):
                 print "Running loop level 1: %d of 256" % i
 		for j in range(256):
                         start_time = time.time()
-                        print "  Running loop level 2: %d of 256 at %0.2f." % (j, time.time())
+                        print "  %d of 256 and %d of 256" % (i, j)
 			for k in range(256):
 				val = neuralNetwork([i/256.0,j/256.0,k/256.0])	# pass BGR value to the NN
 
@@ -66,12 +68,25 @@ def storeBGR2Output(start_index, end_index):
 				AllOutputs.write(str(val))
 				AllOutputs.write("\n")
 
+                        # print "Stats print start at %0.2f" % time.time()
+
                         end_time = time.time()
                         total_step_2_time = end_time - start_time
                         step_2_times.append(total_step_2_time)
-                        # print step_2_times
-                        print "  -- Average time for level 2 array: %0.2f" % (sum(step_2_times) / len(step_2_times))
 
+                        average_time = (sum(step_2_times) / len(step_2_times))
+                        percentage_done = ((j/256.0)*(i+1-start_index)/float(end_index-start_index)) * 100
+                        time_remaining = ((end_time - op_start_time) / percentage_done * 100 / 60) if percentage_done != 0 else 3600
+
+                        print "  -- Average time for level 2 array: %0.2f" % average_time
+                        print "  -- Percentage of total work done : %0.6f percent" % percentage_done
+                        print "  -- Remaining time                : %0.3f minutes" % time_remaining
+
+                        # print "Stats print stop at %0.2f" % time.time()
+
+                # save the input upto the last i in a separate file to enable Ctrl+C'ing this script
+
+                shutil.copyfile(main_file_name, 'backup_' + str(start_index) + '-' + str(i+1) + '.txt')
 
 '''
 This function load the file into the 3D array called NNOutput
